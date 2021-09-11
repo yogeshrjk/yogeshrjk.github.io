3dd02bdbe806006474e934aae967c6041ab9088a
YogeshRajak <yrajak9@gmail.com>
2021-09-11 10:35:41 +0530

    Resume Added

--- assets/js/type.js
+++ assets/js/type.js
@@ -1,5 +1,5 @@
 var typed6 = new Typed('#typed6', {
-    strings: ['\< html \><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< head \><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< title \> Welcome \< title /\><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< head /\><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< body \><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< h1 \> Thank You for visiting me! \< h1 /\><br/> ^1000\n &nbsp;&nbsp;&nbsp; \< body /\><br/> ^1000\n \< html /\>'],
+    strings: ['\< html \><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< head \><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< title \> Welcome \< title /\><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< head /\><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< body \><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< h1 \> Thank You for visiting me! \< h1 /\><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< body /\><br/> ^1000\n \< html /\>'],
     
     typeSpeed: 50,
     backSpeed: 0,
