c76630aac7cba974bd606710c06e2fcdde488242
YogeshRajak <yrajak9@gmail.com>
2021-10-08 01:45:07 +0530

    website modified

--- assets/js/type.js
+++ assets/js/type.js
@@ -1,7 +1,7 @@
 var typed6 = new Typed('#typed6', {
-    strings: ['\< html \><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< head \><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< title \> Welcome \< title /\><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< head /\><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< body \><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< h1 \> Thank You for visiting me! \< h1 /\><br/> ^1000\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \< body /\><br/> ^1000\n \< html /\>'],
+    strings: ['Let\'s bring your ideas to life.'],
     
-    typeSpeed: 50,
+    typeSpeed: 80,
     backSpeed: 0,
     fadeOut: true,
     smartBackspace: true,
