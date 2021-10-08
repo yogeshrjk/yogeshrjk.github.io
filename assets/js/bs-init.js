ca8e198559e7664c5fc02282bce0c4e154fc8665
YogeshRajak <yrajak9@gmail.com>
2021-10-08 09:26:10 +0530

    about section modified

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -10,6 +10,12 @@ if (window.innerWidth < 768) {
 document.addEventListener('DOMContentLoaded', function() {
 	AOS.init();
 
+	var hoverAnimationTriggerList = [].slice.call(document.querySelectorAll('[data-bss-hover-animate]'));
+	var hoverAnimationList = hoverAnimationTriggerList.forEach(function (hoverAnimationEl) {
+		hoverAnimationEl.addEventListener('mouseenter', function(e){ e.target.classList.add('animated', e.target.dataset.bssHoverAnimate) });
+		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
+	});
+
 	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
 	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 	  return new bootstrap.Tooltip(tooltipTriggerEl);
